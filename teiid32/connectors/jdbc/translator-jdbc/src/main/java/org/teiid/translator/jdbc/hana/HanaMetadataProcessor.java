29d0f11c935e32cd3cba14a91ffff509ed008986
yongqian wang <yongqian.wang@CZ1CG1.PRI>
2020-05-13 17:12:14 +0800

    delete all filles

--- teiid32/connectors/jdbc/translator-jdbc/src/main/java/org/teiid/translator/jdbc/hana/HanaMetadataProcessor.java
+++
@@ -1,7 +0,0 @@
-package org.teiid.translator.jdbc.hana;
-
-import org.teiid.translator.jdbc.JDBCMetadataProcessor;
-
-public class HanaMetadataProcessor extends JDBCMetadataProcessor {
-
-}
--- teiid32/runtime/src/test/java/org/teiid/runtime/Funcs.java
+++
@@ -1,9 +0,0 @@
-package org.teiid.runtime;
-
-public class Funcs {
-
-    public static boolean something(long val) {
-        return val > System.currentTimeMillis();
-    }
-
-}
